02d2c92b989c717a3bbfbc24fb65ac4bd8a88b69
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2024-01-26 16:02:43 +0100

    Add setup.py for installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bundle_net',
+      version='0.1',
+      description='BunDLe-Net: a deep learning approach to learning manifolds for worm behavior and neural activity',
+      url='',
+      author='',
+      author_email='',
+      license='MIT',
+      packages=setuptools.find_namespace_packages(),
+      zip_safe=False)
